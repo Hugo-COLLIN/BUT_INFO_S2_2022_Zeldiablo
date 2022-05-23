79c849c1217422650e62ec6dc3899dc387a7c2f0
Rafael Didier Menene Akoo Obono <menene12.akoo@gmail.com>
2022-05-23 22:19:34 +0200

    posMonstrePerso

---
+++ racine_depot/zeldiablo/src/gameLaby/laby/modele/Monstre.java
@@ -0,0 +1,15 @@
+package gameLaby.laby.modele;
+
+import gameLaby.laby.Position;
+
+public class Monstre extends Position {
+    /**
+     * constructeur
+     *
+     * @param dx position selon x
+     * @param dy position selon y
+     */
+    public Monstre(int dx, int dy) {
+        super(dx, dy);
+    }
+}
