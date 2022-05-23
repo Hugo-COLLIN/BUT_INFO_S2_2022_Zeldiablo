bd0b07e3ee74e6227c9b956cde0e61605de8b5ae
Rafael Didier Menene Akoo Obono <menene12.akoo@gmail.com>
2022-05-23 22:03:45 +0200

    monstre

--- racine_depot/zeldiablo/src/gameLaby/laby/view/LabyDessin.java
+++ racine_depot/zeldiablo/src/gameLaby/laby/view/LabyDessin.java
@@ -13,7 +13,6 @@ public class LabyDessin implements DessinJeu {
     @Override
     public void dessinerJeu(Jeu jeu, Canvas canvas) {
         LabyJeu labyJeu = (LabyJeu) jeu;
-
         final GraphicsContext gc = canvas.getGraphicsContext2D();
 
         Labyrinthe labyrinthe = labyJeu.getLaby();
