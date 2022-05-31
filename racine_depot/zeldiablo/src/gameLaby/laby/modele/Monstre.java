65d92b57294ed3be70a9e621c1f6eef7e14c57fa
DARDENNE Gregory <gdardenne0@gmail.com>
2022-05-31 14:26:36 +0200

    Ajout de etreAttire

--- racine_depot/zeldiablo/src/gameLaby/laby/modele/Monstre.java
+++ racine_depot/zeldiablo/src/gameLaby/laby/modele/Monstre.java
@@ -11,5 +11,41 @@ public class Monstre extends Position {
         super(dx, dy);
     }
 
+    public void etreAttire(Personnage p, String action) {
+        int distanceInitiale = this.distanceEntre(p);
+        int prochainX = this.x;
+        int prochainY = this.y;
+        switch (action) {
+            case Labyrinthe.HAUT:
+                // on monte une ligne
+                prochainY--;
+                break;
+            case Labyrinthe.BAS:
+                // on descend une ligne
+                prochainY++;
+                break;
+            case Labyrinthe.DROITE:
+                // on augmente colonne
+                prochainX++;
+                break;
+            case Labyrinthe.GAUCHE:
+                // on augmente colonne
+                prochainX--;
+                break;
+        }
+        Position futurPos = new Position(prochainX, prochainY);
+        int distanceApresDeplacement = this.distanceEntre(new Position(prochainX, prochainY));
+
+        if(distanceInitiale > distanceApresDeplacement) {
+            this.x = futurPos.x;
+            this.y = futurPos.y;
+        } else {
+            if (this.x - futurPos.getX() == 1) this.x++;
+            else if (this.x - futurPos.getX() == -1) this.x--;
+            else if (this.y - futurPos.getY() == 1) this.y++;
+            else if (this.y - futurPos.getY() == -1) this.y--;
+        }
+    }
+
 
 }
